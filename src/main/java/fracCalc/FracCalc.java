96bb8c0e7462319a2126c792daaf7075d42d7eab
s-limag <s-limag@53S500042222.it.bsd405.org>
2019-12-01 21:14:35 -0800

    Checkpoint 3

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -82,102 +82,132 @@ public class FracCalc {
 		else {
 			whole = secondNumber;
 		}
-//		String fwhole = "0";
-//		String fnumerator = "0";
-//		String fdenominator = "1";
-//		int flength= firstNumber.length();
-//		int fi =0;
-//		int fj=0;
-//		while (fi <flength) {
-//			if (firstNumber.substring(fi,fi+1).equals("_")) {
-//				 fj=fi+1;
-//				fi = flength+1;	
-//			}
-//			fi++;
-//		}
-//		int fk =0; 
-//		int fp=0;
-//		while (fk <flength) {
-//			if (firstNumber.substring(fk,fk+1).equals("/")) {
-//				 fp=fk+1;
-//				fk = flength+1;	
-//			}
-//			fk++;
-//		}
-//		//numerator can only be one digit right now. SO FIX THAT !!!
-//		if (fj==0) {
-//			fj=1;
-//		}
-//		if (fp==0) {
-//			fp=1;
-//		}
-//		if (firstNumber.substring(fj-1, fj).equals("_")) {
-//			fwhole = firstNumber.substring(0, fj-1);
-//			fnumerator = firstNumber.substring(fj, fp-1);
-//			fdenominator = firstNumber.substring(fp);
-//		}
-//		else if (firstNumber.substring(fp-1, fp).equals("/")) {
-//			fnumerator = firstNumber.substring(0,fp-1);
-//			fdenominator = firstNumber.substring(fp);
-//		}
-//		else {
-//			fwhole = firstNumber;
-//		}
-//		int iWhole= 0;
-//		int iDen = 0;
-//		int ifNum = 0;
-//		int isNum = 0;
-//		int fNum = 0;
-//		if (operators.substring(0).equals("+")) {
-//			iWhole = Integer.parseInt(fwhole)+ Integer.parseInt(whole);
-//			iDen = Integer.parseInt(fdenominator)*Integer.parseInt(denominator);
-//			ifNum = Integer.parseInt(fnumerator)*Integer.parseInt(denominator);
-//			isNum = Integer.parseInt(numerator)*Integer.parseInt(fdenominator);
-//			fNum=ifNum + isNum;
-//			iWhole = iWhole + fNum/iDen;
-//			fNum = fNum%iDen;
-//				}
-//		if (operators.substring(0).equals("+") && fNum==0) {
-//			System.out.println(iWhole);
-//		}
-//		else if (operators.substring(0).equals("+") && iWhole==0) {
-//		System.out.println(fNum + "/"+iDen);
-//		}
-//		else if (operators.substring(0).equals("+")){
-//		System.out.println(iWhole+"_"+fNum + "/"+iDen);
-//		}
-//		if (operators.substring(0).equals("-")) {
-//			iWhole = Integer.parseInt(fwhole)- Integer.parseInt(whole);
-//			iDen = Integer.parseInt(fdenominator)*Integer.parseInt(denominator);
-//			ifNum = Integer.parseInt(fnumerator)*Integer.parseInt(denominator);
-//			isNum = Integer.parseInt(numerator)*Integer.parseInt(fdenominator);
-//			fNum=ifNum - isNum;
-//			iWhole = iWhole + fNum/iDen;
-//			fNum = fNum%iDen;
-//				}
-//		if (operators.substring(0).equals("-") && fNum==0) {
-//			System.out.println(iWhole);
-//		}
-//		else if (operators.substring(0).equals("-") && iWhole==0) {
-//		System.out.println(fNum + "/"+iDen);
-//		}
-//		else if (operators.substring(0).equals("-")) {
-//		System.out.println(iWhole+"_"+fNum + "/"+iDen);
-//		}
+		String fwhole = "0";
+		String fnumerator = "0";
+		String fdenominator = "1";
+		int flength= firstNumber.length();
+		int fi =0;
+		int fj=0;
+		while (fi <flength) {
+			if (firstNumber.substring(fi,fi+1).equals("_")) {
+				 fj=fi+1;
+				fi = flength+1;	
+			}
+			fi++;
+		}
+		int fk =0; 
+		int fp=0;
+		while (fk <flength) {
+			if (firstNumber.substring(fk,fk+1).equals("/")) {
+				 fp=fk+1;
+				fk = flength+1;	
+			}
+			fk++;
+		}
+		//numerator can only be one digit right now. SO FIX THAT !!!
+		if (fj==0) {
+			fj=1;
+		}
+		if (fp==0) {
+			fp=1;
+		}
+		if (firstNumber.substring(fj-1, fj).equals("_")) {
+			fwhole = firstNumber.substring(0, fj-1);
+			fnumerator = firstNumber.substring(fj, fp-1);
+			fdenominator = firstNumber.substring(fp);
+		}
+		else if (firstNumber.substring(fp-1, fp).equals("/")) {
+			fnumerator = firstNumber.substring(0,fp-1);
+			fdenominator = firstNumber.substring(fp);
+		}
+		else {
+			fwhole = firstNumber;
+		}
+		int iWhole= 0;
+		int iDen = 0;
+		int ifNum = 0;
+		int isNum = 0;
+		int fNum = 0;
+		String answer= "0";
+		int sign = 1;
+		int fSign = 1;
+		int sSign = 1;				
+		if (firstNumber.charAt(0) == '-') { 
+			sign = -1;
+			 fSign=-1;}
+		if (secondNumber.charAt(0) == '-') {
+			sign *= (-1);
+			 sSign = -1;
+		}
+		int fw = Math.abs(Integer.parseInt(fwhole));
+		int w = Math.abs(Integer.parseInt(whole));
+		int fd = Integer.parseInt(fdenominator);
+		int d = Integer.parseInt(denominator);
+		int fn =Math.abs(Integer.parseInt(fnumerator));
+		int n = Math.abs(Integer.parseInt(numerator));
+		
+		if (operators.substring(0).equals("+")) {
+			iWhole = fw*fSign+ w*sSign;
+			iDen = fd*d;
+			ifNum = fn*d*fSign;
+			isNum = (n*fd)*sSign;
+			fNum=ifNum*fSign + isNum*fSign;
+			iWhole = iWhole + fNum/iDen;
+			fNum = fNum%iDen;
+				}
 
-//	int firstNum = Integer.parseInt(firstNumber);
-//	int secondNum = Integer.parseInt(secondNumber);
+		if (operators.substring(0).equals("-")) {
+			iWhole = fw*fSign- w*sSign;
+			iDen = fd*d;
+			ifNum = fn*d*fSign;
+			isNum = n*fd*sSign;
+			fNum=ifNum*fSign - isNum*sSign;
+			iWhole = iWhole + fNum/iDen;
+			fNum = fNum%iDen;
+				}
+		
+		if (operators.substring(0).equals("*")) {
+			
+			ifNum = fd*fw+fn;
+			isNum = d*w + n;
+			fNum=ifNum*isNum;
+			iDen = fd*d;
+			iWhole=fNum/iDen;
+			fNum=fNum%iDen;
+			iWhole = iWhole*sign;
+			if (iWhole==0) {
+				fNum= fNum*sign;
+			}
+		}
 
-		String seperate = ("whole:"+whole+" numerator:"+numerator+" denominator:"+denominator);
+		if (operators.substring(0).equals("/")) {
+			ifNum = fd*fw+fn;
+			isNum = d*w + n;
+			fNum=ifNum*d;
+			iDen = fd*isNum;
+			iWhole=fNum/iDen;
+			fNum = fNum;
+			iDen = iDen;
+			fNum=fNum%iDen;
+			iWhole = iWhole*sign;
+			if (iWhole==0) {
+				fNum= fNum*sign;
+			}
+		}
+		
+		char op = operators.charAt(0);
+		if ((op == '+'|| op=='/'|| op=='-'|| op=='*') && fNum==0) {
+		 answer = (iWhole+"");
+		}
+		else if ((op == '+'|| op=='/'|| op=='-'|| op=='*') && iWhole==0) {
+		answer =(fNum + "/"+iDen);
+		}
+		else if ((op == '+'|| op=='/'|| op=='-'|| op=='*')) {
+		answer = (iWhole+"_"+Math.abs(fNum) + "/"+iDen);
+		}
 
 
-		return seperate;
+		return answer;
 	}
-//	public static int commonDenominator(int fd, int sd, int fn) {
-//		int x = 0;
-//		x = fd*sd;
-//		
-//		return x ;
-//	}
 
 }
